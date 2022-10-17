a864264067bb29c757ab81fbbd72aca3ed72486b
不将就 <2581548799@qq.com>
2022-10-17 20:41:57 +0800

    添加QQ邮件发送功能

---
+++ src/main/java/com/designpattern/factory/Rectangle.java
@@ -0,0 +1,9 @@
+package com.designpattern.factory;
+
+public class Rectangle implements Shape {
+
+   @Override
+   public void draw() {
+      System.out.println("Inside Rectangle::draw() method.");
+   }
+}
